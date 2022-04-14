c52848db882a7fd6b667f3914622eb8ab854206e
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-14 12:48:10 +0200

    eslint install and configured

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
